0f41873e47e8e067d2b174cf1b4aa048c6edd604
maecapozzi <maecapozzi@gmail.com>
2019-12-21 11:06:54 -0500

    Seed database with users

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,10 @@ import "reflect-metadata";
 import { createConnection } from "typeorm";
 import * as express from "express";
 import * as bodyParser from "body-parser";
+import * as faker from "faker";
 import { Request, Response } from "express";
 import { routes } from "./routes";
+import { User } from "./entity/User";
 
 createConnection()
   .then(async connection => {
@@ -34,8 +36,36 @@ createConnection()
       );
     });
 
-    // setup express app here
-    // ...
+    const buildFakeUsers = () => {
+      let users = [];
+
+      for (let index = 0; index < 1000; index++) {
+        users.push({
+          email: faker.internet.email(),
+          firstName: faker.name.firstName(),
+          lastName: faker.name.lastName()
+        });
+      }
+
+      return users;
+    };
+
+    const users = buildFakeUsers();
+
+    users.forEach(user => {
+      let userRecord = new User();
+
+      userRecord.email = user.email;
+      userRecord.firstName = user.firstName;
+      userRecord.lastName = user.lastName;
+
+      const userRepository = connection.getRepository(User);
+
+      userRepository
+        .save(user)
+        .then(user => console.log("user has been saved: ", user))
+        .catch(error => console.log("Cannot save. Error: ", error));
+    });
 
     // start express server
     app.listen(3000);
